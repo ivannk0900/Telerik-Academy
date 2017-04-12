d52afaa36ed2d8b227a9acf739ea5a5b31b43a9b
Ivan Kitanovski <ivannk0900@gmail.com>
2017-04-12 15:49:05 +0300

    added sorting tasks

---
+++ DSA/SortingAlgorithms/Sorting/Sort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new List<int>() { 3, 5, 1 };
+
+            //var sorter = new SelectionSorter<int>();
+            //sorter.Sort(myList);
+
+            var quickSorter = new Quicksorter<int>();
+            quickSorter.Sort(myList);
+
+
+
+            foreach (var el in myList)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
---
+++ DSA/SortingAlgorithms/Sorting/Sort/QuickSorter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sort
+{
+    public class Quicksorter<T> : ISorter<T> where T : IComparable<T>
+    {
+        public void Sort(IList<T> collection)
+        {
+            QuickSort(collection, 0, collection.Count - 1);
+        }
+
+        private void QuickSort(IList<T> collection, int left, int right)
+        {
+            var pivot = collection[(left + right) / 2];
+
+            var i = left;
+            var j = right;
+
+            while (i <= j)
+            {
+                while (collection[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (collection[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i > j)
+                {
+                    continue;
+                }
+
+                var temp = collection[i];
+                collection[i] = collection[j];
+                collection[j] = temp;
+
+                i++;
+                j--;
+            }
+
+            if (left < j)
+            {
+                this.QuickSort(collection, left, j);
+            }
+
+            if (i < right)
+            {
+                this.QuickSort(collection, i, right);
+            }
+
+
+        }
+    }
+}
